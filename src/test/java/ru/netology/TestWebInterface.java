46d349f71029c2e23502c0b30027759acb733692
Юлия Алексина <uliyaalexina@mail.ru>
2022-09-09 07:19:08 -0700

    Fixed src/

---
+++ src/test/java/ru/netology/TestWebInterface.java
@@ -0,0 +1,73 @@
+package ru.netology;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestWebInterface {
+
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setupAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setup() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void teardown() {
+        driver.quit();
+        driver = null;
+
+    }
+
+
+    @Test
+    void test1() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[type ='text' ]")).sendKeys("Иванов Андрей");
+        driver.findElement(By.cssSelector("[type ='tel' ]")).sendKeys("+79124325678");
+        driver.findElement(By.cssSelector(".checkbox__box")).click();
+        driver.findElement(By.cssSelector(".button__text")).click();
+        String text = driver.findElement(By.className("paragraph")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+    }
+    @Test
+    void test2() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[type ='text' ]")).sendKeys("Петров Иван");
+        driver.findElement(By.cssSelector("[type ='tel' ]")).sendKeys("+79215674980");
+        driver.findElement(By.cssSelector(".checkbox__box")).click();
+        driver.findElement(By.cssSelector(".button__text")).click();
+        String text = driver.findElement(By.className("paragraph")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+    }
+    @Test
+    void test3() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[type ='text' ]")).sendKeys("Андреев Николай");
+        driver.findElement(By.cssSelector("[type ='tel' ]")).sendKeys("+79657568934");
+        driver.findElement(By.cssSelector(".checkbox__box")).click();
+        driver.findElement(By.cssSelector(".button__text")).click();
+        String text = driver.findElement(By.className("paragraph")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+    }
+}
